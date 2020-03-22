94047893467c8f5d727b6891913acaab88c31cb8
Ertugrul Karademir <ekarademir@gmail.com>
2020-03-22 18:47:04 +0000

    Recursive iteration implemented

--- probability_book/permutation.py
+++ probability_book/permutation.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
+from pprint import pprint
 
+
+def permutations(arr):
+    _permutations = []
+    def permute(arr, head):
+        if len(arr) == 1:
+            _permutations.append(head + arr)
+
+        for i in range(len(arr)):
+            permute(arr[:i] + arr[i + 1:], head + [arr[i]])
+    permute(arr, [])
+    return _permutations
 
 
 if __name__ == "__main__":
-    pass
+    pprint(permutations(list(range(5))))
